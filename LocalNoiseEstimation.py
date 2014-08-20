928e36d6b74b4b824a84ed343433f6300a0daeb0
haiderriazkhan <haider.riaz@mail.mcgill.ca>
2014-08-20 15:15:42 -0400

    Add LocalNoiseEstimation

---
+++ LocalNoiseEstimation.py
@@ -0,0 +1,23 @@
+'''
+Created on Aug 18, 2014
+
+@author: haiderriaz
+'''
+
+import array
+from array import zeros
+import math 
+
+def main(ima,x,y,z):
+    
+    minimum = reduce(min, ima)
+    
+    if (minimum < 0):
+        ima = map(lambda x: x - minimum, ima)
+     
+    p1 =  math.pow(2 , math.ceil(math.log(x, 2)))
+    p2 =  math.pow(2 , math.ceil(math.log(y, 2)))         
+    p3 =  math.pow(2 , math.ceil(math.log(z, 2)))        
+                
+    # zeros pading 
+    
